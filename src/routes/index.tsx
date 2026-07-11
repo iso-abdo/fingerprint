e8d8f3965bf8593d0601ffc3901f501b8539221b
iso-abdo <isoemo79@gmail.com>
2026-07-11 17:32:24 +0000

    primary/5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -35,10 +35,10 @@ function Index() {
 >
   
   {/* التعديل: الارتفاع مخصص pt-[85] لجعل التموضع دقيقاً حسب رغبتك */}
-    <div className="absolute inset-0 flex justify-center items-start pt-70">
+    <div className="absolute inset-0 flex justify-center items-start pt-85">
     <Link
       to="/request"
-      className="inline-flex items-center gap-2 rounded-lg bg-primary/20 backdrop-blur-md border border-primary/30 px-8 py-3.5 text-foreground font-semibold shadow-lg shadow-primary/5 hover:bg-primary/40 transition transform hover:scale-105"
+      className="inline-flex items-center gap-2 rounded-lg bg-primary/0 backdrop-blur-md border border-primary/30 px-8 py-3.5 text-foreground font-semibold shadow-lg shadow-primary/1 hover:bg-primary/5 transition transform hover:scale-105"
     >
       ابــدأ الآن
       <ArrowLeft className="h-4 w-4" />
